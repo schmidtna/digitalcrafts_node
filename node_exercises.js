17e2492af6a9c2268c9a3e2d04a221fe8beb800e
Nick Schmidt <nick@schmidtyanimation.com>
2018-05-17 04:35:47 -0500

    node exercises

--- node_exercises.js
+++ node_exercises.js
@@ -1,21 +1,42 @@
-var readline = require('readline');
-var rl = readline.createInterface({
+const readline = require('readline');
+const request = require('request');
+const fs = require('fs');
+const dns = require('dns');
+
+
+const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
-var fs = require('fs');
+// open a file, convert it to caps, save it out to another file.
 
-rl.question('Enter a file to read : ', function(answer) {
-    fs.readFile(answer, function (error, buffer) {
+rl.question('Enter a file to read : ', function(inputFileName) {
+    fs.readFile(inputFileName, function (error, fileContents) {
         if (error) {
             console.error(error.message);
             return;
         }
-        var fileData = buffer.toString();
-        var upperCase = fileData.toUpperCase();
-        console.log('File Data: ' + upperCase);
+        var fileTxt = fileContents.toString();
+        var fileTxtUpper = fileTxt.toUpperCase();
+        fs.writeFile('output.txt', fileTxtUpper, function (error) {
+            if (error) {
+                console.error(error.message);
+                return;
+            }
+            console.log('File saved: ', 'output.txt');
+        })
         rl.close();
     })
 });
 
+//dns request
+
+rl.question("Enter domain name: ", function(enterDomainName) {
+    dns.lookup(enterDomainName, (err, address, family) => {
+        if (err) throw err;
+            
+        console.log('IP address: %j ', address,);
+        rl.close();
+    })
+});
