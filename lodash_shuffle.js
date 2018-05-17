58508c850ae4ff8ca699b87f31813770ef1f0b32
Nick Schmidt <nick@schmidtyanimation.com>
2018-05-17 05:09:11 -0500

    working with npm modules

---
+++ lodash_shuffle.js
@@ -0,0 +1,10 @@
+const _ = require('lodash');
+const fp = require('lodash/fp');
+
+var numberArray = [1, 2, 3, 4, 5, 6];
+
+console.log(numberArray);
+
+shuffledArray = _.shuffle(numberArray);
+
+console.log(shuffledArray);
