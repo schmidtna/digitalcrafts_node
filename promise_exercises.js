c48f44aacd4712b2f18cccf217cbb564a02b9819
Nick Schmidt <nick@schmidtyanimation.com>
2018-05-29 04:23:30 -0500

    promise exer progress

--- promise_exercises.js
+++ promise_exercises.js
@@ -1,50 +1,37 @@
 const rp = require('request-promise');
+const fse = require('fs-extra');
 
-var urls = [
-    'https://en.wikipedia.org/wiki/Futures_and_promises',
-    'https://en.wikipedia.org/wiki/Continuation-passing_style',
-    'https://en.wikipedia.org/wiki/JavaScript',
-    'https://en.wikipedia.org/wiki/Node.js',
-    'https://en.wikipedia.org/wiki/Google_Chrome'
-  ];
-var i;
+// var urls = [
+//     'https://en.wikipedia.org/wiki/Futures_and_promises',
+//     'https://en.wikipedia.org/wiki/Continuation-passing_style',
+//     'https://en.wikipedia.org/wiki/JavaScript',
+//     'https://en.wikipedia.org/wiki/Node.js',
+//     'https://en.wikipedia.org/wiki/Google_Chrome'
+//   ];
+// var i;
 
-var urlPromiseArray = urls.map(function (url) {
-    return rp(url)
-})
-
-// for (i = 0; i < urls.length; i++) {
-//     urlPromiseArray.push(rp(urls[i]));
-// };
-
-Promise.all(urlPromiseArray).then(function() {
-    console.log('Job is done ');
-})
-    .catch(function (error) {
-        console.error('There was an error');
-    });
+// var urlPromiseArray = urls.map(function (url) {
+//     return rp(url)
+// })
 
 
+// Promise.all(urlPromiseArray).then(function() {
+//     console.log('Job is done ');
+// })
+//     .catch(function (error) {
+//         console.error('There was an error');
+//     });
 
+//ex 2
+function saveWebPage(url, filename) {
+    return new Promise((resolve, reject) => {
+        urlGrab = rp(url)
+        resolve(urlGrab);
+        console.log('URL grabbed successfully : ' + url)
+    })
+    .then(function (){
+        fse.writeFile(filename, )
+    })
+}
 
-
-    
-
-
-    // .then(requestSuccess)
-    // .catch(requestError);
-
-    // function requestSuccess (html) {
-    // console.log('I got the html and its this many characters: ' + html.length)
-    // }
-
-    // function requestError () {
-    // console.error("something went wrong requesting !!!")
-    // }
-
-
-
-
-// step 1 list of urls
-//convert that into a list of promises
-//send that list of promises to promise.all
+saveWebPage('https://google.com', 'test.txt')
